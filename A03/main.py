9088d3d89a002f288d1c7210db3458a97c76b2c2
chloequinto <cquinto@stevens.edu>
2020-02-06 17:35:01 -0500

    finishing up with comments

--- A03/main.py
+++ A03/main.py
@@ -4,15 +4,9 @@ CS - 581 WS
 Online Social Networks 
 Assignment 3 
 I pledge my honor that I have abided by the Stevens Honor System - Chloe Quinto
-"""
 
-"""
-To Do 
-1. Complete OutputVideoCount
-2. Complete OutputLikesCount
-3. Complete OutputDislikesCount
-2. Complete function writeToCSV
-
+main.py - A Python program that makes an API call to youtube given a query and number of results. The program 
+also writes the result to CSV and 
 """
 
 from apiclient.discovery import build 
@@ -26,79 +20,49 @@ YouTubeObject = build(API_NAME, API_VERSION, developerKey = API_KEY)
 
 
 def OutputVideoCount(resultVidCount):
-    '''
-    Description: 
-        Function sorts and prints out title, videocount and id onto the console based on the highest view count. 
-
-    Parameters: 
-        resultsVidCount: A dictionary with keys as the videoCount and values as the title, videoCount and ID 
-
-    Returns: 
-        str: returns str to be printed  
-    '''
+    #Function sorts and prints out title, videocount and id onto the console based on the highest view count. 
     print("--------- Ranking View Count -------------")
     viewCount = sorted(resultVidCount.items(), key=lambda x: int(x[0]), reverse=True)
     rank = 1 
     for key, value in viewCount: 
         print("Rank: " + str(rank) +  " Title:  " +str(value.get("title")) + " Video ID: "  + str(value.get("videoId")) + " ViewCount: " + str(key))
         rank+=1 
+        if rank == 6:
+            break
     print("\n")
     return None
 
 def OutputLikesCount(resultLikes):
-    '''
-    Description: 
-        Function sorts and prints out title, videocount and id onto the console based on the highest likes 
-
-    Parameters: 
-        resultLikes: A dictionary with keys as the likes and values as the title, videoCount and ID 
-
-    Returns: 
-        None
-    '''
+    #Function sorts and prints out title, videocount and id onto the console based on the highest percentage of likes. 
     print("--------- Ranking Like Count -------------")
-    likeCount = sorted(resultLikes.items(), key=lambda x: int(x[0]), reverse=True)
+    likeCount = sorted(resultLikes.items(), key=lambda x: float(x[0])*100, reverse=True)
     rank = 1 
     for key, value in likeCount: 
         percent = round((value.get("percentLikes") * 100), 5)
         print("Rank: " + str(rank) +  " Title:  " +str(value.get("title")) + " Video ID: "  + str(value.get("videoId")) + " Percent Likes: " + str(percent))
         rank+=1 
+        if rank == 6: 
+            break
     print("\n")
     return None
 
 def OutputDislikesCount(resultDislikes): 
-    '''
-    Description: 
-        Function sorts and prints out title, videocount and id onto the console based on the highest dislikes 
-
-    Parameters: 
-        resultLikes: A dictionary with keys as the dislikes and values as the title, videoCount and ID 
-
-    Returns: 
-        None
-    '''
+    #Function sorts and prints out title, videocount and id onto the console based on the highest percentage of dislikes. 
     print("--------- Ranking Dislikes Count -------------")
-    dislikeCount = sorted(resultDislikes.items(), key=lambda x: int(x[0]), reverse=True)
+    dislikeCount = sorted(resultDislikes.items(), key=lambda x: float(x[0])*100, reverse=True)
     rank = 1 
     for key, value in dislikeCount: 
         percent = round((value.get("percentDislikes") * 100), 5)
         print("Rank: " + str(rank) +  " Title:  " +str(value.get("title")) + "  Video ID:  "  + str(value.get("videoId")) + "  Percent Dislikes:  " + str(percent))
         rank+=1 
+        if rank == 6: 
+            break
     print("\n")
     return None
 
 def ranking(results, numResults): 
-    '''
-    Description: 
-        Function takes in results and number of results and extracts title, videocount, id, likes, dislikes etc. 
+    #Function takes the results from the API call and number of searches and partitions data 
 
-    Parameters: 
-        results: JSON response from API query 
-        numResults: number of queries the user wants 
-
-    Returns: 
-        dict: returns dictionary of partitioned data based on videoCount, likes, and dislikes 
-    '''
     printVidCount, printLikes, printDislikes = {}, {}, {}
     items = results["items"] # A list of activities, or events, that match the request criteria.
     for i in range(len(items)): 
@@ -118,37 +82,33 @@ def ranking(results, numResults):
 
 
         printVidCount[viewCount] = {"title": title, "videoId" : videoId, "viewCount ": viewCount}
-        printLikes[likes] = {"title": title, "videoId": videoId, "percentLikes":percentLikes}
-        printDislikes[dislikes] = {"title": title, "videoId":videoId, "percentDislikes":percentDislikes}
+        printLikes[percentLikes] = {"title": title, "videoId": videoId, "percentLikes":percentLikes}
+        printDislikes[percentDislikes] = {"title": title, "videoId":videoId, "percentDislikes":percentDislikes}
     
-    OutputVideoCount(printVidCount)
-    OutputLikesCount(printLikes)
-    OutputDislikesCount(printDislikes)
 
-    return None
+    return OutputVideoCount(printVidCount), OutputLikesCount(printLikes), OutputDislikesCount(printDislikes)
 
 
 def writeToCSV(results):
-    header = []
-    for key, value in results.items(): 
+    #Function takes the results from the API call and writes to CSV
+    content, header, vals = [], [], []
+    
+    for key in results: 
         header.append(key)
+        vals.append(results[key])
+
     with open("results.csv", "w") as csvfile: 
         filewriter = csv.writer(csvfile)
         filewriter.writerow(header)
+        filewriter.writerow(vals)
+    return None 
+
+
 
 def searchYoutube(term, max_results): 
-    '''
-    Description: 
-        Function takes in results and number of results and makes an API call. 
-
-    Parameters: 
-        term: query from the user 
-        max_results: number of results the user wants 
-
-    Returns: 
-        JSON Response: returns json response from API call 
-    '''
+    # Function takes in the term queried and number of results and makes an API call. 
     response = YouTubeObject.search().list(q = term, part="id, snippet", maxResults = max_results).execute()
+
     print("Search Term: " + term)
     print("Search Max: " + max_results)
     print("Results:\n" + str(response))
@@ -159,7 +119,13 @@ def searchYoutube(term, max_results):
 if __name__ == "__main__":
 
     query = input("What would you like to search?\n")
+
+    if len(query)== 0:
+        raise Exception("Sorry, you need to search at least something.")
+
     numResults = input("How many results would you like?\n")
+    if int(numResults) == 0 or int(numResults) < 0: 
+        raise Exception("Sorry, we need you to choose a number > 0.")
 
     print("-----------Searching-----------")
 
