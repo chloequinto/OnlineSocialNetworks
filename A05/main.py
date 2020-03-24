862eaaf81ddee1cfd458d457fd8a27cd871f031e
chloequinto <cquinto@stevens.edu>
2020-03-24 18:20:04 -0400

    some progress

--- A05/main.py
+++ A05/main.py
@@ -1,38 +1,64 @@
 import csv
 import networkx as nx 
 
-class Graph: 
-    def __init__(self, reviewer, reviewee, trustLevel):
-        self.reviewer = reviewer
-        self.reviewee = reviewee
-        self.trustLevel = trustLevel 
+class CSVdata: 
+    def __init__(self, edges, selfLoops, TotEdges): 
+        self.edges = edges
+        self.selfLoops = selfLoops
+        self.TotEdges = TotEdges
 
+def findSelfLoops(G):
+    nodes_in_selfloops = []
+    for i, v in G.edges(): 
+        if i == v: 
+            nodes_in_selfloops.append(i)
+    return len(nodes_in_selfloops)
 
-class Info: 
-    def __init__ (self, selfLoops): 
-        self.selfLoops = 1
 
 def openAndPrint(fileName): 
+    global selfLoops
+    G = nx.Graph()
     with open(fileName) as csvFile: 
         for row in csvFile: 
             row = row.replace("\n", "") # get rid of new line breaks 
             line = row.split(",") # split into an array 
-    
+            G.add_edge(line[0], line[1], weight = line[2])
 
-            Graph.reviewer = line[0]
-            Graph.reviewee = line[1]
-            Graph.trustLevel = line[2]
+    CSVdata.edges = len(G.edges())
 
-            if (Graph.reviewer == Graph.reviewee): 
-                # this is a self loop 
-                print("We found a self loop!")
-                Info.selfLoops += 1 
- 
-    # print(Info())
-    return None
+    CSVdata.selfLoops = findSelfLoops(G)
+
+    CSVdata.TotEdges = CSVdata.edges - CSVdata.selfLoops
+
+    # val = [for i in G.edges.data]
+
+    pos = 0 
+    neg = 0
+    for i in G.edges.data(): 
+        if i[2].get("weight") == '1': 
+            pos += 1
+        else: 
+            neg += 1 
+
+
+    CSVdata.trustEdges = pos
+    CSVdata.distrustEdges = neg 
+
+def printResults(): 
+    print("Edges in network: ", CSVdata.edges)
+    print("Self-Loops: ", CSVdata.selfLoops )
+    print("Edges used - TotEdges: ", CSVdata.TotEdges)
+    print("Trust Edges: ",  CSVdata.trustEdges)
+    print("Distrust Edges: ",  CSVdata.distrustEdges)
+
 if __name__ == "__main__":
 
     print("=====================================")
-    fileName = input("Enter the name of the file: \n")
+    '''
+    Row looks like: 221363,261079,1
+    '''
+    # fileName = input("Enter the name of the file: \n")
+    fileName = "epinions96.csv"
     openAndPrint(fileName)
+    printResults()
     print("=====================================")
