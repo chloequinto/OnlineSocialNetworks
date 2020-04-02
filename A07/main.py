cfcda6bab0781ff39c4cade6b23e65786aa2ea89
chloequinto <cquinto@stevens.edu>
2020-04-02 14:42:38 -0400

    hit rate limit

--- A07/main.py
+++ A07/main.py
@@ -3,11 +3,13 @@ Chloe Quinto
 
 To Run: type into the terminal `python3 main.py`
 
+Dependencies: 
+    - Install Tweepy 
 
 TODO 
 [X] Prompt the user for a Twitter User Screen Name.  If the user enters STOP, end the program with an appropriate message and stop.
-[] Use the Twitter Screen Name to retrieve the following from the Twitter User Account and display them on the console with appropriate labels. 
-[] Print the text of the Twitter User Account's most recent tweet, with appropriate label.
+[X] Use the Twitter Screen Name to retrieve the following from the Twitter User Account and display them on the console with appropriate labels. 
+[X] Print the text of the Twitter User Account's most recent tweet, with appropriate label.
 [] Print the screen names of thefirst 10followers of the Twitter User Account, with appropriate label. 
 [] Prompt the user for the next Twitter User Screen Name, unless they enter STOP.
 '''
@@ -16,38 +18,111 @@ TODO
 import tweepy 
 
 # Authentication Keys
-CONSUMER_KEY = ""
-CONSUMER_KEY_SECRET = ""
-ACCESS_TOKEN = ""
-ACCESS_TOKEN_SECRET = ""
+CONSUMER_KEY = "TAYPHnUgbeX7MCAOZMWNLQXim"
+CONSUMER_KEY_SECRET = "imhLVU2GxuFiSSShHPxh9xZO2jSbPzC9YWnpzZBngxd4AEIYUv"
+ACCESS_TOKEN = "1245765406960357377-yrAnszN73w0bxiHEFqGEIiIWuZgnMq"
+ACCESS_TOKEN_SECRET = "kqaxUFfLYGGVTcH7BdMB11Zmlsj4kxPwJ8lK5u6OdKmWw"
 
 # Authenticate 
-authenticate = tweepy.OAUthHandler(CONSUMER_KEY, CONSUMER_KEY_SECRET)
-authentivate.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+authenticate = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_KEY_SECRET)
+authenticate.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 # Avoid going over Twitter's Rate limits 
 API = tweepy.API(authenticate, wait_on_rate_limit = True, wait_on_rate_limit_notify = True)
 
+# Global Variables 
+screen_name = ""
+
+# Rate Limit 
+sleep_on_rate_limit = False
+
 def getUser(user_screen_name): 
     '''
         Function gets information about a twitter user account information 
-        Such as: 
-        - User Name 
-        - User Screen Name 
-        - User ID 
-        - User Description 
-        - Number of Followers 
+        Such as: User Name, User Screen Name, User ID, User Description, Number of Followers 
     '''
+
+    global screen_name
     try: 
-        twitter_user = api.get_user(user_screen_name)
+        twitter_user = API.get_user(user_screen_name)
+        
+        print("\n============================")
+        # User Name 
+        user_name = twitter_user.name
+
+        # User Screen Name 
+        screen_name = twitter_user.screen_name
+
+        # User ID 
+        user_id = twitter_user.id
+
+        # User Description 
+        user_description = twitter_user.description
+
+        # Number of Followers
+        num_followers =  twitter_user.followers_count
+
+        print("User Name:", user_name)
+        print("Screen Name:", screen_name)
+        print("User ID:", user_id)
+        print("User Description:", user_description)
+        print("Number of Followers:", num_followers)
+        print("============================\n")
+
     except: 
         print("[ERROR] Could not get twitter user information")
+    return None
 
-def getMostRecentTweet()
+def getMostRecentTweet():
+    '''
+        Function print the user's most recent tweet with appropriate label 
+    '''
+    global screen_name
+    try: 
+        print("============================")
+        recent_tweet = API.get_user(screen_name = screen_name)
+        print("Most Recent Tweet by " +  screen_name  + " \n" +  recent_tweet.status.text)
+        print("============================\n")
+    except: 
+        print("[ERROR] Coult not get most recent twitter information")
+
+
+def getFollowers(): 
+    '''
+        Function gets screen names of the first 10 followers of the Twitter User Account 
+    '''
+    global screen_name 
+    print("============================")
+    print("Screen Names of First 10 Followers of " + screen_name)
+    try: 
+        count = 0
+        for users in tweepy.Cursor(API.followers, id = screen_name, count = 10 ).items(): 
+            if count == 10: 
+                break 
+            count += 1
+            print(users.screen_name)
+    except: 
+        print("[ERROR] Could not get followers")
+
+    print("============================\n")
+
+    # print(followers_list)
+
+
+
 
 
 if __name__ == "__main__":
-    user_screen_name = input("What Twitter User Account do you want to analyze? Type \"STOP\" to end program")
-    if (user_screen_name == "STOP"): 
-        return 
-    getUser(user_screen_name)
+    print("============================")
+    user_screen_name = input("What Twitter User Account do you want to analyze? Type \"STOP\" to end program\n")
+
+    while user_screen_name != "STOP": 
+        getUser(user_screen_name)
+        getMostRecentTweet()
+        getFollowers()
+        user_screen_name = input("What Twitter User Account do you want to analyze? Type \"STOP\" to end program\n")
+
+    if user_screen_name == "STOP": 
+        exit()
+    
+    print("============================")
