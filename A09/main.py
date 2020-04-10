747641d3feeddb1ddc6a4df3715c2fcd08ee6f4d
chloequinto <cquinto@stevens.edu>
2020-04-09 21:29:35 -0400

    added some readability

--- A09/main.py
+++ A09/main.py
@@ -30,10 +30,7 @@ Note:
 '''
 
 
-
-# In[1]:
-
-
+# Importing Libraries 
 import pandas as pd 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -42,30 +39,17 @@ import seaborn as sns
 import warnings
 warnings.filterwarnings('ignore')
 
-
-# In[2]:
-
-
+# Let's read in the file 
 fb_data = pd.read_csv('FB_data.csv')
 
-
-# In[3]:
-
-
+# Let's look at the top of the csv to get a better look 
 fb_data.head()
 
-
-# In[4]:
-
-
-len(fb_data)
+# How big is the data set 
+print(len(fb_data))
 
 
-# ## Current Demographic 
-
-# In[5]:
-
-
+# Let's take a look at the current age dmographic 
 plt.hist(fb_data['age'], color="green", ec="white", bins=100)
 plt.title("Current Age Demographic")
 plt.xlabel("Age")
@@ -73,13 +57,11 @@ plt.ylabel("Amount")
 plt.savefig('output/0_AgeDemographic.png')
 plt.show()
 
+# we see that there's outliers where users are greater than 105, these are more likely fake ages 
 
 
-# Outliers such as accounts greater than 105 are most likely fake ages.
-
-# In[6]:
-
-
+# since there are "NA"s in gender, we need to drop rows where it includes it 
+# Here we are going to look at the gender demographic 
 fb_data.dropna(inplace=True)
 plt.hist(fb_data["gender"])
 plt.title("Current gender demographic")
@@ -90,20 +72,18 @@ plt.show()
 print("Gender \n" + str(fb_data["gender"].value_counts()))
 
 
-# --- 
+'''
+Dunbar's Number
 
-# # Dunbar's Number
-# 
-# For this assignment, I want to focus on Dunbar's number. The theory goes that we can only really maintain about 150 connections at once. But does this still hold true for today's world of social media. 
-# 
-# I explore this topic with the facebook dataset and go over a few questions such as: 
-# 1. Number of friends per user 
-# 2. Friends count vs likes received 
+For this assignment, I want to focus on Dunbar's number. The theory goes that we can only really maintain about 150 connections at once. But does this still hold true for today's world of social media. 
 
-# ## Number of Friends
+I explore this topic with the facebook dataset and go over a few questions such as: 
+1. Number of friends per user 
+2. Friends count vs likes received 
+'''
 
-# In[7]:
 
+# Let's see the number of friends
 plt.hist(fb_data["friend_count"], color="green", ec="white", bins=100)
 plt.ylabel("Amount of Users")
 plt.xlabel("Number of Friends")
@@ -112,12 +92,12 @@ plt.xlim(0,1100)
 plt.savefig('output/2_numberOfFriends.png')
 plt.show()
 
+'''
+For me personally, when I add friends on FB I don't necessarily interact with them so they really aren't "connections" per se. 
+Let's see if there is any correlation to the number of friends that one has to the number of likes they receive.
+We should expect to see a positive correlation between the number of friends one has with the number of likes a person gets. 
+'''
 
-# For me personally, when I add friends on FB I don't necessarily interact with them so they really aren't "connections" per se. Let's see if there is any correlation to the number of friends that one has to the number of likes they receive.
-# 
-# We should expect to see a positive correlation between the number of friends one has with the number of likes a person gets. 
-
-# In[8]:
 
 plt.scatter(fb_data["friend_count"], fb_data["likes_received"])
 plt.title("Friends Count vs Likes Received")
@@ -128,9 +108,7 @@ plt.ylabel("Likes Received")
 plt.savefig('output/3_friendsCountVsLikesReceived.png')
 plt.show()
 
-# In[9]:
-
-
+# Let's verify the results via stats - original linear regression 
 results = sm.OLS(fb_data["likes_received"],sm.add_constant(fb_data["friend_count"])).fit()
 print(results.summary())
 
@@ -143,14 +121,10 @@ print("R2: ", results.rsquared)
 
 # Looks like there is a weak positive correlation between  likes received and a user's friend count 
 
-# In[11]:
-
-
+# Let's look at the linear regression line 
 print("y="+str(results.params[1]) + "*x + " + str(results.params[0]))
 
-
-# In[12]:
-
+# Let's plot the same thing as before but with a linear regression line 
 plt.scatter(fb_data["friend_count"], fb_data["likes_received"])
 plt.plot(fb_data["friend_count"], results.params[1]*fb_data["friend_count"]+results.params[0], color="red", linewidth=4)
 plt.xlim(0, 150)
@@ -161,44 +135,47 @@ plt.ylabel("Likes Received")
 plt.savefig('output/4_friendsCountVsLikesReceived.png')
 plt.show()
 
+'''
+We can't confidently say that the more friends you have the more likes you receive. 
+This may be due to the nature of Facebook. Most users, including me, use it as a way of messaging 
+communication rather than reading posts and liking them. As opposed to instagram, where the intention is to give and 
+receive likes and comments. 
 
-# We can't confidently say that the more friends you have the more likes you receive. This may be due to the nature of Facebook. Most users, including me, use it as a way of messaging communication rather than reading posts and liking them. As opposed to instagram, where the intention is to give and receive likes and comments. 
+''' 
 
-# --- 
-# Another question to look at it: 
-#     
-# ### If you're more likely to initiate friendships, are you more likely receive likes?
-
-# In[13]:
-
+'''
+Another question to look at it: 
+    
+If you're more likely to initiate friendships, are you more likely receive likes?
+'''
 
 sns.relplot(x="friendships_initiated", y="likes_received", data=fb_data, hue="gender")
 plt.savefig('output/5_InitiateFriendsVsLikesOnFacebook.png')
 plt.plot()
 
+'''
+That's interesting. The more friends you initiate, the less likes you get. 
 
-# That's interesting. The more friends you initiate, the less likes you get. 
-# 
-# Dunbar's theory says that you should have 500 acquaintances and 1500 people you can recognize. The numbers on the x-axis go all the way up to 4000 which is beyond the people you could possibly recognize. After 1500 friends, they are not a strong connection. 
-# 
+Dunbar's theory says that you should have 500 acquaintances and 1500 people you can recognize. The numbers on the x-axis go all the way up to 4000 which is 
+beyond the people you could possibly recognize. After 1500 friends, they are not a strong connection. 
 
-# In[14]:
+Let's just verify the results with stats: 
+'''
+
 
 
 results = sm.OLS(fb_data["likes_received"],fb_data["friendships_initiated"]).fit()
 print(results.summary())
 
-
-# In[19]:
-
-
 print('R2: ', results.rsquared)
 print('Parameters: ', results.params)
 
 
-# ---
-# Another question we could ask: 
-# ## Do females or males get more likes on facebook? 
+'''
+Another question we could ask: 
+Do females or males get more likes on facebook? 
+'''
+
 
 # In[16]:
 
